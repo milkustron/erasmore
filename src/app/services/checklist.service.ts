fb3c00fc3721fb88bf3059aa6c75c2d8a8667538
Furkan Celik <furkan.rwth-aachen.de>
2025-04-22 12:56:38 +0100

    Remove test description

--- src/app/services/checklist.service.ts
+++ src/app/services/checklist.service.ts
@@ -28,206 +28,206 @@ export class ChecklistService {
           "stage": "Settling In",
           "task": "Schedule an appointment for NIE",
           "link": "https://sede.administracionespublicas.gob.es/pagina/index/directorio/icpplus",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 2,
           "stage": "Settling In",
           "task": "Prepare the necessary documents for NIE",
           "link": "https://www.inclusion.gob.es/web/migraciones/w/certificado-de-registro-de-ciudadano-de-la-union-europea-hi-101-",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 3,
           "stage": "Settled",
           "task": "Schedule an appointment for empadronamiento",
           "link": " https://www.laspalmasgc.es/es/otras-secciones/cita-previa/",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 4,
           "stage": "Settled",
           "task": "Prepare the necessary documents for empadronamiento",
           "link": "https://sedeelectronica.laspalmasgc.es/web/fichaAsunto.do?opcion=0&asas_ide_asu=857&ruta=/web/asuntosMasUsuales.do?opcion",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 5,
           "stage": "Preparation",
           "task": "Check passport/ID validity and apply if necessary",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 6,
           "stage": "Preparation",
           "task": "Check EHIC card validity and apply if necessary",
           "link": "https://employment-social-affairs.ec.europa.eu/policies-and-activities/moving-working-europe/eu-social-security-coordination/european-health-insurance-card_en",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 7,
           "stage": "Preparation",
           "task": "Check EHIC card validity and apply if necessary",
           "link": "https://employment-social-affairs.ec.europa.eu/policies-and-activities/moving-working-europe/eu-social-security-coordination/european-health-insurance-card_en",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 8,
           "stage": "Preparation",
           "task": "Check your debit card limits in the destination country's currency and apply for a better one if necessary",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 9,
           "stage": "Preparation",
           "task": "If you have insurance, check its coverage and apply for additional coverage if necessary",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 10,
           "stage": "Preparation",
           "task": "Check your roaming package included in your current telecom plan",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 11,
           "stage": "Preparation",
           "task": "Check your roaming package included in your current telecom plan",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 12,
           "stage": "Preparation",
           "task": "Finalize the learning agreement",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 13,
           "stage": "Settling In",
           "task": "Submit the certificate of arrival template to the university if a specific one is required",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 14,
           "stage": "Settling In",
           "task": "Collect the certificate of arrival from the coordinator and submit it to your home university",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 15,
           "stage": "Preparation",
           "task": "Find interesting accommodation offers",
           "link": "https://internacional.ulpgc.es/en/movilidad-incoming/estudiantes-incoming/study-at-the-ulpgc-incoming/exchange-students/accommodation/",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 16,
           "stage": "Trip Day",
           "task": "Check the best transport connections to your place of residence",
           "link": "https://www.grancanaria.com/turismo/en/create-your-own-holiday/around-the-island/have-you-just-arrived-at-gran-canaria/",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 17,
           "stage": "Trip Day",
           "task": "Contact the landlord or the reception of your accommodation",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 18,
           "stage": "Settled",
           "task": "Follow local organizator of erasmus acitivities: ESN",
           "link": "linktr.ee/ESNLasPalmas",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 19,
           "stage": "Settled",
           "task": "Follow local organizator of erasmus acitivities: Erasmus Life",
           "link": "linktr.ee/erasmuslifelaspalmas",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 20,
           "stage": "Settled",
           "task": "Apply for free public transport: Wawa Joven or Bono Residente",
           "link": "https://www.guaguas.com/tarifas-carnets",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 21,
           "stage": "Settled",
           "task": "Apply for a residential discount for travel",
           "link": "https://www.laspalmasgc.es/es/ayuntamiento/certificados/",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 22,
           "stage": "Departure",
           "task": "Submit the certificate of attendance template to the university if a specific one is required",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 23,
           "stage": "Departure",
           "task": "Collect the certificate of attendance from the coordinator and submit it to your home university",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 24,
           "stage": "Preparation",
           "task": "Verify the necessity of applying for a visa in your case",
           "link": "https://www.exteriores.gob.es/Embajadas/seul/en/ServiciosConsulares/Paginas/Consular/Visados-nacionales-Informacion-general.aspx",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 25,
           "stage": "Preparation",
           "task": "Sign the grant agreement at your home university",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 26,
           "stage": "Preparation",
           "task": "Polish your skills in the language of your erasmus studies",
           "link": "https://erasmus-plus.ec.europa.eu/resources-and-tools/online-language-support",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 27,
           "stage": "Settling In",
           "task": "Make changes to the Learning Agreement if necessary",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 28,
           "stage": "Departure",
           "task": "Collect the Transcript of Records from the host university",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 29,
           "stage": "Departure",
           "task": "Submit the Transcript of Records to your home university",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 30,
           "stage": "Departure",
           "task": "Arrange the return of your security deposit from your accommodation",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 31,
           "stage": "Departure",
           "task": "Clean your place of residence before departure",
-          "description": "test description: "
+          "description": " "
         },
         {
           "id": 32,
           "stage": "Departure",
           "task": "Arrange the time and method of handing over the keys with the landlord",
-          "description": "test description: "
+          "description": " "
         }
       ]
   }
@@ -238,6 +238,10 @@ export class ChecklistService {
 
   getChecklistByStage(stage: string): ChecklistItem[] {
     return this.data.todo.filter(item => item.stage === stage);
+  }
+
+  getChecklistItemById(id: number): ChecklistItem {
+    return this.data.todo.filter(item => item.id === id)[0];
   }
 
   constructor() { }
